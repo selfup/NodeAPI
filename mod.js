a4994ef7e844a353c269ee0e8e09475869d33140
selfup <boudinot.regis@yahoo.com>
2016-01-23 15:35:06 -0700

    module exports ES6 style

--- mod.js
+++ mod.js
@@ -1,13 +1,13 @@
 'use strict'
 
-exports.rootPage = (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-}
-
-exports.findAll = (req, res) => {
-    res.send([{name:'mod1'}, {name:'mod2'}, {name:'mod3'}])
-}
-
-exports.findById = (req, res) => {
-    res.send({id:req.params.id, name: "The Name", description: "description"})
+module.exports = {
+  rootPage: (req, res) => {
+      res.sendFile(__dirname + '/public/index.html')
+  },
+  findAll: (req, res) => {
+      res.send([{name:'mod1'}, {name:'mod2'}, {name:'mod3'}])
+  },
+  findById: (req, res) => {
+      res.send({id:req.params.id, name: "The Name", description: "description"})
+  }
 }
